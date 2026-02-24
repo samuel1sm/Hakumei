e53872f8d87ee033e076b83e0d57c13f62e470e9
Samuel Martins <fsamuelsmartins@gmail.com>
2026-02-24 15:43:18 -0300

    feat: adding header texts

--- Hakumei/Views/Home/HomeView.swift
+++ Hakumei/Views/Home/HomeView.swift
@@ -5,10 +5,22 @@ struct HomeView: View {
     var body: some View {
 		NavigationStack {
 			VStack {
-				VStack {
+				VStack(alignment: .leading, spacing: 16){
 					Text("かな · HAKUMEI")
-				}.frame(maxWidth: .infinity)
-					.background(.backgroundHeader)
+						.font(.hakumei.appLabel)
+						.tracking(3.0)
+						.foregroundStyle(.accentGold)
+					Text("Good morning,\nAlex さん")
+						.foregroundStyle(.textInverted)
+						.font(.hakumei.title1)
+					Text("Your Japanese journey continues.")
+						.foregroundStyle(.textMutedOnDark)
+						.font(.hakumei.bodySmall)
+				}
+				.frame(maxWidth: .infinity, alignment: .leading)
+				.frame(height: 220)
+				.padding(.horizontal)
+				.background(.backgroundHeader)
 			}
         }
     }
