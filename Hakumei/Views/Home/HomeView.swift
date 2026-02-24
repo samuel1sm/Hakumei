1e2a1e78bcc2a0afedd41242d21c9fcabf8a079f
Samuel Martins <fsamuelsmartins@gmail.com>
2026-02-24 16:02:29 -0300

    feat: aling header's text and adding kanji

--- Hakumei/Views/Home/HomeView.swift
+++ Hakumei/Views/Home/HomeView.swift
@@ -5,19 +5,26 @@ struct HomeView: View {
     var body: some View {
 		NavigationStack {
 			VStack {
-				VStack(alignment: .leading, spacing: 16){
-					Text("かな · HAKUMEI")
-						.font(.hakumei.appLabel)
-						.tracking(3.0)
-						.foregroundStyle(.accentGold)
-					Text("Good morning,\nAlex さん")
-						.foregroundStyle(.textInverted)
-						.font(.hakumei.title1)
-					Text("Your Japanese journey continues.")
-						.foregroundStyle(.textMutedOnDark)
-						.font(.hakumei.bodySmall)
+				ZStack(alignment: .trailing) {
+					Text("日")
+						.font(.hakumei.headerKanji)
+						.foregroundStyle(.white.opacity(0.04))
+						.offset(.init(width: 0, height: -16))
+
+					VStack(alignment: .leading, spacing: 16){
+						Text("かな · HAKUMEI")
+							.font(.hakumei.appLabel)
+							.tracking(3.0)
+							.foregroundStyle(.accentGold)
+						Text("Good morning,\nAlex さん")
+							.foregroundStyle(.textInverted)
+							.font(.hakumei.title1)
+						Text("Your Japanese journey continues.")
+							.foregroundStyle(.textMutedOnDark)
+							.font(.hakumei.bodySmall)
+					}
+					.frame(maxWidth: .infinity, alignment: .leading)
 				}
-				.frame(maxWidth: .infinity, alignment: .leading)
 				.frame(height: 220)
 				.padding(.horizontal)
 				.background(.backgroundHeader)
