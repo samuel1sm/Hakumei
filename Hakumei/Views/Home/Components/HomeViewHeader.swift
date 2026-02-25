db433437930be084575d74121d44b07c3dd41c7e
Samuel Martins <fsamuelsmartins@gmail.com>
2026-02-25 10:21:05 -0300

    feat: creating home header view

---
+++ Hakumei/Views/Home/Components/HomeViewHeader.swift
@@ -0,0 +1,48 @@
+//
+//  HomeViewHeader.swift
+//  Hakumei
+//
+//  Created by Samuel Martins on 25/02/26.
+//
+
+
+import SwiftUI
+
+struct HomeViewHeader: View {
+	let greeting: String
+	let userName: String
+	
+	var body: some View {
+		ZStack(alignment: .trailing) {
+			Text("日")
+				.font(.hakumei.headerKanji)
+				.foregroundStyle(.white.opacity(0.04))
+				.offset(.init(width: 0, height: -16))
+
+			VStack(alignment: .leading, spacing: 16){
+				Text("かな · HAKUMEI")
+					.font(.hakumei.appLabel)
+					.tracking(3.0)
+					.foregroundStyle(.accentGold)
+				Text("\(greeting),\n\(userName) さん")
+					.foregroundStyle(.textInverted)
+					.font(.hakumei.title1)
+				Text("Your Japanese journey continues.")
+					.foregroundStyle(.textMutedOnDark)
+					.font(.hakumei.bodySmall)
+			}
+			.frame(maxWidth: .infinity, alignment: .leading)
+		}
+		.frame(height: 220)
+		.padding(.horizontal)
+		.background(.backgroundHeader)
+	}
+}
+
+#Preview("HomeViewHeader") {
+	HomeViewHeader(
+		greeting: "Good morning",
+		userName: "Alex"
+	)
+}
+
--- Hakumei/Views/Home/HomeView.swift
+++ Hakumei/Views/Home/HomeView.swift
@@ -5,29 +5,11 @@ struct HomeView: View {
     var body: some View {
 		NavigationStack {
 			VStack(alignment: .leading, spacing: 20) {
-				ZStack(alignment: .trailing) {
-					Text("日")
-						.font(.hakumei.headerKanji)
-						.foregroundStyle(.white.opacity(0.04))
-						.offset(.init(width: 0, height: -16))
-
-					VStack(alignment: .leading, spacing: 16){
-						Text("かな · HAKUMEI")
-							.font(.hakumei.appLabel)
-							.tracking(3.0)
-							.foregroundStyle(.accentGold)
-						Text("Good morning,\nAlex さん")
-							.foregroundStyle(.textInverted)
-							.font(.hakumei.title1)
-						Text("Your Japanese journey continues.")
-							.foregroundStyle(.textMutedOnDark)
-							.font(.hakumei.bodySmall)
-					}
-					.frame(maxWidth: .infinity, alignment: .leading)
-				}
-				.frame(height: 220)
-				.padding(.horizontal)
-				.background(.backgroundHeader)
+				HomeViewHeader(
+					greeting: "Good morning",
+					userName: "Alex"
+				)
+				
 				VStack(alignment: .leading, spacing: 16) {
 					Text("Your progress")
 						.font(.hakumei.sectionTitle)
@@ -49,8 +31,6 @@ struct HomeView: View {
     }
 }
 
-
 #Preview {
-    HomeView()
+	HomeView()
 }
-
